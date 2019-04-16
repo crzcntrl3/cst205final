6f19b7492c2d03089e03a8aa6ccd3d3b3522301e
Peter Zed <zedislast@gmail.com>
2019-04-16 07:18:29 -0700

    initial commit

---
+++ final.py
@@ -0,0 +1,33 @@
+# Guess That Picture game
+# Peter Zed, Nicholas Hinds, Andy Alameddine, Pooya Beheshti, Jordan Cruz
+
+class Question():
+  def __init__(self, question, answers, image_path):
+    self.question = question
+    self.answers = answers
+    scrumble_image(image_path)
+    
+  def scrumble_image(self, image_path):
+    self.scrumbled_image = image_path
+    
+  def get_answer_values(self):
+    return [answer.value for answer in self.answers]
+    
+  def get_correct_answer(self):
+    return next((answer for answer in self.answers if answer.is_correct), None)
+
+class Answer():
+  def __init__(self, value, is_correct=False):
+    self.value = value
+    self.is_correct = is_correct
+
+def game():
+  question1 = Question("Who dis?", [
+    Answer("Andy"),
+    Answer("Pooya"),
+    Answer("Nicholas", True),
+  ])
+  
+  printNow(question1.question)
+  for index, answer in enumerate(question1.get_answer_values()):
+    print "%s. %s" % (str(index + 1), answer)
