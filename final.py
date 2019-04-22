b8c791ae47ce59f3e1096c65fb4db8b2596952dc
Andy <andyalam9@gmail.com>
2019-04-21 18:28:20 -0700

    base quiz taking implementation

--- final.py
+++ final.py
@@ -1,13 +1,53 @@
 # Guess That Picture game
 
 
+class Quiz():
+    def __init__(self, questions):
+        self.questions = questions
+        self.correct = []
+        self.incorrect = []
+    
+    def start(self):
+        # Loop through questions and get answers for each
+        for question in self.questions:
+            self.printQuestion(question)
+            response = self.handleInput(question)
+            answer = question.get_answer(response - 1)
+            if answer.is_correct:
+                self.correct.append(question)
+            else:
+                self.incorrect.append(question)
+        # Question taking over, display some results
+        print('Thank you for taking the quiz, here are your results!')
+        print('Correct: ' + str(len(self.correct)))
+        print('Incorrect: ' + str(len(self.incorrect)))
+
+
+    def printQuestion(self, question):
+        printNow(question.question)
+        for index, answer in enumerate(question.get_answer_values()):
+            print("%s. %s" % (str(index + 1), answer))
+    
+    def handleInput(self, question):
+        while True:
+            response = requestString("Enter your answer")
+            if int(response) in range(1, len(question.answers) + 1):
+                response = int(response)
+                print('\n')
+                break
+            else:
+                print("Please enter a valid response")
+        
+        return response
+
 class Question():
-    def __init__(self, question, answers, image_path):
+    def __init__(self, question, answers, image_path=''):
         self.question = question
         self.answers = answers
-        scramble_image(image_path)
+        self.scramble_image(image_path)
 
     def scramble_image(self, image_path):
+        return
         # this should scramble the image using the path in the argument
         # self.scrambled_image =
 
@@ -15,7 +55,12 @@ class Question():
         return [answer.value for answer in self.answers]
 
     def get_correct_answer(self):
-        return next((answer for answer in self.answers if answer.is_correct), None)
+        for answer in self.answers:
+            if answer.is_correct:
+                return answer
+
+    def get_answer(self, index):
+        return self.answers[index]
 
 
 class Answer():
@@ -25,12 +70,23 @@ class Answer():
 
 
 def game():
-    question1 = Question("Who dis?", [
-        Answer("Andy"),
-        Answer("Pooya"),
-        Answer("Nicholas", True),
+    quiz = Quiz([
+        Question("Who dis?", [
+            Answer(value="Andy"),
+            Answer(value="Pooya"),
+            Answer(value="Nicholas", is_correct=True),
+        ]),
+        Question("Who dis?", [
+            Answer(value="Andy"),
+            Answer(value="Pooya"),
+            Answer(value="Nicholas", is_correct=True),
+        ]),
+        Question("Who dis?", [
+            Answer(value="Andy"),
+            Answer(value="Pooya"),
+            Answer(value="Nicholas", is_correct=True),
+        ])
     ])
+    quiz.start()
 
-    printNow(question1.question)
-    for index, answer in enumerate(question1.get_answer_values()):
-        print "%s. %s" % (str(index + 1), answer)
+game()
