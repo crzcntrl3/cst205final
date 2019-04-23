548580e9ad8e7982ea1fdb1739e80a0c04e3135e
Peter Zed <zedislast@gmail.com>
2019-04-22 19:20:13 -0700

    added image scrambling

--- final.py
+++ final.py
@@ -105,15 +105,15 @@ GAME_OPTIONS = [
             },
             {
                 'value': 'What is human',
-                'image_path': 'pictures/kidneys.jpg',
+                'image_path': 'pictures/bladder.jpg',
                 'answers': [
                     {
                         'value': 'kidneys',
-                        'is_correct': True,
+                        'is_correct': False,
                     },
                     {
                         'value': 'bladder',
-                        'is_correct': False,
+                        'is_correct': True,
                     },
                     {
                         'value': 'intestines',
@@ -126,6 +126,128 @@ GAME_OPTIONS = [
 ]
 
 
+class Puzzle:
+    def __init__(self, image_path):
+        self.pic = makePicture("%s/%s" % (PATH, image_path))
+
+    # Makes the individual slice of the original image
+    def slicing(self, pic, startX, startY, endX, endY):
+        x = abs(startX-endX)
+        y = abs(startY-endY)
+        slicedPic = makeEmptyPicture(x, y)
+        # creates arrays to hold the information of the original and empty images
+        originalPixels = []
+        slicedPixels = []
+        # grabs pixel information of the original image and store it in its corresponding array
+        for originalX in range(startX, endX):
+            for originalY in range(startY, endY):
+                originalP = getPixel(self.pic, originalX, originalY)
+                originalPixels.append(originalP)
+        # grabs pixel information of the empty image and store it in its corresponding array
+        for slicedX in range(0, x):
+            for slicedY in range(0, y):
+                slicedP = getPixel(slicedPic, slicedX, slicedY)
+                slicedPixels.append(slicedP)
+        # uses the zip() to combine the two arrays and steps through each pair in the array
+        for combinePixels in zip(originalPixels, slicedPixels):
+            # grabs each element of the zipped array
+            origPix = combinePixels[0]
+            slicedPix = combinePixels[1]
+            # sets the color to the empty image
+            setColor(slicedPix, getColor(origPix))
+        return slicedPic
+
+    # slices the image into 12 peices
+    def puzzleSlices(self, pic):
+        w = getWidth(self.pic)
+        h = getHeight(self.pic)
+        sectionArray = []
+
+        # First Quater
+        section1 = self.slicing(self.pic, 0, 0, w/3, h/4)
+        section2 = self.slicing(self.pic, (w/3), 0, (w*2)/3, h/4)
+        section3 = self.slicing(self.pic, (w*2)/3, 0, w, h/4)
+        # Second Quater
+        section4 = self.slicing(self.pic, 0, h/4, w/3, (h*2)/4)
+        section5 = self.slicing(self.pic, (w/3), h/4, (w*2)/3, (h*2)/4)
+        section6 = self.slicing(self.pic, (w*2)/3, h/4, w, (h*2)/4)
+        # Third Quater
+        section7 = self.slicing(self.pic, 0, (h*2)/4, w/3, (h*3)/4)
+        section8 = self.slicing(self.pic, (w/3), (h*2)/4, (w*2)/3, (h*3)/4)
+        section9 = self.slicing(self.pic, (w*2)/3, (h*2)/4, w, (h*3)/4)
+        # Fourth Quater
+        section10 = self.slicing(self.pic, 0, (h*3)/4, w/3, h)
+        section11 = self.slicing(self.pic, (w/3), (h*3)/4, (w*2)/3, h)
+        section12 = self.slicing(self.pic, (w*2)/3, (h*3)/4, w, h)
+
+        # creates an arrays of the image slices
+        sectionArray.append(section1)
+        sectionArray.append(section2)
+        sectionArray.append(section3)
+        sectionArray.append(section4)
+        sectionArray.append(section5)
+        sectionArray.append(section6)
+        sectionArray.append(section7)
+        sectionArray.append(section8)
+        sectionArray.append(section9)
+        sectionArray.append(section10)
+        sectionArray.append(section11)
+        sectionArray.append(section12)
+
+        return sectionArray
+
+    # Copy function
+    def arrange(self, section, whole, placeX, placeY):
+        w = getWidth(section)
+        h = getHeight(section)
+
+        sectionPixels = []
+        wholePixels = []
+
+        for secX in range(0, w):
+            for secY in range(0, h):
+                secP = getPixel(section, secX, secY)
+                sectionPixels.append(secP)
+        for wholeX in range(placeX, placeX+w):
+            for wholeY in range(placeY, placeY+h):
+                wholeP = getPixel(whole, wholeX, wholeY)
+                wholePixels.append(wholeP)
+        for combine in zip(sectionPixels, wholePixels):
+            secPix = combine[0]
+            wholePix = combine[1]
+            setColor(wholePix, getColor(secPix))
+        return wholePix
+
+    # Creates the puzzle image
+
+    def scrambled(self):
+        w = getWidth(self.pic)
+        h = getHeight(self.pic)
+        section = self.puzzleSlices(self.pic)
+
+        # Creates an array of 12 numbers 0-11 with no repeats
+        r = random.sample(range(12), 12)
+
+        puzzle = makeEmptyPicture(w, h)
+        self.arrange(section[r[0]], puzzle, 0, 0)
+        self.arrange(section[r[1]], puzzle, w/3, 0)
+        self.arrange(section[r[2]], puzzle, (w*2)/3, 0)
+
+        self.arrange(section[r[3]], puzzle, 0, h/4)
+        self.arrange(section[r[4]], puzzle, w/3, h/4)
+        self.arrange(section[r[5]], puzzle, (w*2)/3, h/4)
+
+        self.arrange(section[r[6]], puzzle, 0, (h*2)/4)
+        self.arrange(section[r[7]], puzzle, w/3, (h*2)/4)
+        self.arrange(section[r[8]], puzzle, (w*2)/3, (h*2)/4)
+
+        self.arrange(section[r[9]], puzzle, 0, (h*3)/4)
+        self.arrange(section[r[10]], puzzle, w/3, (h*3)/4)
+        self.arrange(section[r[11]], puzzle, (w*2)/3, (h*3)/4)
+
+        return puzzle
+
+
 def clip(source, start, end):
     target = makeEmptySound(end - start, int(getSamplingRate(source)))
     targetIndex = 0
@@ -134,6 +256,7 @@ def clip(source, start, end):
         setSampleValueAt(target, targetIndex, value)
         targetIndex = targetIndex + 1
     return target
+
 
 def copy(source, target, start):
     targetLength = getLength(target)
@@ -144,21 +267,22 @@ def copy(source, target, start):
         value = getSampleValueAt(source, index)
         setSampleValueAt(target, targetIndex, value)
     return target
- 
+
+
 class Quiz():
     def __init__(self, questions):
         self.questions = questions
         self.correct = []
         self.incorrect = []
-        self.makeGameSounds()
+        self.game_sounds = self.makeGameSounds()
 
     def start(self):
         # Loop through questions and get answers for each
         for question in self.questions:
             self.printQuestion(question)
             response = self.handleInput(question)
-            answer = question.get_answer(response - 1)
-            
+            answer = question.getAnswer(response - 1)
+
             if answer.is_correct:
                 self.handleCorrectAnswer(question)
             else:
@@ -171,9 +295,10 @@ class Quiz():
             self.handleLose()
 
     def printQuestion(self, question):
+        show(question.getScrambledImage())
         printNow(question.question)
-        for index, answer in enumerate(question.get_answer_values()):
-            print("%s. %s" % (str(index + 1), answer))
+        for index, answer in enumerate(question.getAnswerValues()):
+            print("%s. %s" % (index + 1, answer))
 
     def handleInput(self, question):
         printNow("Enter your answer")
@@ -187,40 +312,43 @@ class Quiz():
                 printNow("Please enter a valid response")
 
         return response
-        
+
     def isLastQuestion(self):
         return len(self.correct) + len(self.incorrect) == len(self.questions)
 
     def handleCorrectAnswer(self, question):
         self.correct.append(question)
-        
-        if not self.isLastQuestion():
-          printNow("You are right!\n")
-          reaction = random.choice(self.gameSounds['yays'])
-          play(reaction)
+        show(question.getUnscrambledImage())
 
+        if not self.isLastQuestion():
+            printNow("You are right!\n")
+            reaction = random.choice(self.game_sounds['yays'])
+            play(reaction)
 
     def handleIncorrectAnswer(self, question):
         self.incorrect.append(question)
+        show(question.getUnscrambledImage())
 
         if not self.isLastQuestion():
             printNow("Wrong answer!\n")
-            reaction = random.choice(self.gameSounds['oohs'])
+            reaction = random.choice(self.game_sounds['oohs'])
             play(reaction)
-        
+
     def handleWin(self):
-        play(self.gameSounds['win'][0])
+        play(self.game_sounds['win'][0])
         showInformation("You win!\n"
                         "Correct answers: %s\n"
-                        "Incorrect answers: %s" % (len(self.correct), len(self.incorrect))
-                       )
-        
+                        "Incorrect answers: %s" % (
+                            len(self.correct), len(self.incorrect))
+                        )
+
     def handleLose(self):
-        play(self.gameSounds['lose'][0])
+        play(self.game_sounds['lose'][0])
         showInformation("You lose!\n"
                         "Correct answers: %s\n"
-                        "Incorrect answers: %s" % (len(self.correct), len(self.incorrect))
-                       )
+                        "Incorrect answers: %s" % (
+                            len(self.correct), len(self.incorrect))
+                        )
 
     def makeGameSounds(self):
         samplingRate = 44100
@@ -228,15 +356,17 @@ class Quiz():
         yays = makeSound("%s/sounds/yays.wav" % PATH)
         thanks = makeSound("%s/sounds/thanks.wav" % PATH)
         applause = makeSound("%s/sounds/applause.wav" % PATH)
-        win = makeEmptySound(getLength(applause) + getLength(thanks), samplingRate)
+        win = makeEmptySound(getLength(applause) +
+                             getLength(thanks), samplingRate)
         win = copy(applause, win, 0)
         win = copy(thanks, win, getLength(applause))
         nooo = makeSound("%s/sounds/nooo.wav" % PATH)
-        lose = makeEmptySound(getLength(nooo) + getLength(thanks), samplingRate)
+        lose = makeEmptySound(
+            getLength(nooo) + getLength(thanks), samplingRate)
         lose = copy(nooo, lose, 0)
         lose = copy(thanks, lose, getLength(nooo))
-        
-        self.gameSounds = {
+
+        return {
             "yays": [
                 clip(yays, 1998, 103563),
                 clip(yays, 106893, 206460)
@@ -255,7 +385,7 @@ class Quiz():
                 lose,
             ],
             "win": [
-               win,
+                win,
             ],
         }
 
@@ -264,21 +394,22 @@ class Question():
     def __init__(self, question, answers, image_path=''):
         self.question = question
         self.answers = answers
-        self.scramble_image(image_path)
+        self.image_path = image_path
 
-    def scramble_image(self, image_path):
-        return
+    def getScrambledImage(self):
+        pic = Puzzle(self.image_path)
+        picture = pic.scrambled()
+        return picture
 
-    def get_answer_values(self):
+    def getAnswerValues(self):
         return [answer.value for answer in self.answers]
 
-    def get_correct_answer(self):
-        for answer in self.answers:
-            if answer.is_correct:
-                return answer
-
-    def get_answer(self, index):
+    def getAnswer(self, index):
         return self.answers[index]
+        
+    def getUnscrambledImage(self):
+        return makePicture("%s/%s" % (PATH, self.image_path))
+    
 
 
 class Answer():
@@ -290,7 +421,7 @@ class Answer():
 def chooseGameTopic():
     topics = [option['topic'] for option in GAME_OPTIONS]
 
-    printNow("Choose topic:")        
+    printNow("Choose topic:")
     for index, topic in enumerate(topics):
         printNow("%s. %s" % (index + 1, topic))
 
@@ -300,11 +431,13 @@ def chooseGameTopic():
             break
     return int(response) - 1
 
+
 def intro():
     intro = makeSound("%s/sounds/intro.wav" % PATH)
     play(intro)
     showInformation("Welcome to the quiz")
-    
+
+
 def game():
     intro()
 
@@ -313,7 +446,8 @@ def game():
 
     # load questions
     questions = GAME_OPTIONS[topic]['questions']
-    quiz = Quiz([Question(question['value'], [Answer(value=answer['value'], is_correct=answer['is_correct']) for answer in question['answers']]) for question in questions])
+    quiz = Quiz([Question(question['value'], [Answer(value=answer['value'], is_correct=answer['is_correct'])
+                                              for answer in question['answers']], question['image_path']) for question in questions])
     quiz.start()
 
 
